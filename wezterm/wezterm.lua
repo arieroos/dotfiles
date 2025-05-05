32fb2d687f06ef8314dc9e963a9c6acf01fe25dc
arie <ariechildofgod@gmailcom>
2025-05-05 20:19:13 +0200

    Decrease font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,6 +11,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Night Owl (Gogh)"
 config.font = wezterm.font("ComicShannsMono Nerd Font")
+config.font_size = 11
 
 config.use_fancy_tab_bar = false
 
