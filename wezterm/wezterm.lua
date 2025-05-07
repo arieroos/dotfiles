56e53f239425f2ee5fb45e4a3d702bcb42268f1b
arie <ariechildofgod@gmailcom>
2025-05-07 11:34:19 +0200

    Add a way to easily resize panes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,19 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local actions = wezterm.action
+
 local windows = require("windows")
 
 wezterm.on("gui-startup", function(cmd)
 	windows.open_window(cmd)
+end)
+
+wezterm.on("update-right-status", function(window, _)
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
 end)
 
 -- This will hold the configuration.
@@ -17,8 +27,36 @@ config.use_fancy_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
 config.keys = {
-	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+	{
+		key = "r",
+		mods = "LEADER",
+		action = actions.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "LeftArrow", action = actions.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = actions.AdjustPaneSize({ "Left", 1 }) },
+
+		{ key = "RightArrow", action = actions.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = actions.AdjustPaneSize({ "Right", 1 }) },
+
+		{ key = "UpArrow", action = actions.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = actions.AdjustPaneSize({ "Up", 1 }) },
+
+		{ key = "DownArrow", action = actions.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = actions.AdjustPaneSize({ "Down", 1 }) },
+
+		-- Cancel the mode by pressing escape
+		{ key = "Escape", action = "PopKeyTable" },
+	},
 }
 
 return config
