d5ad651faf99f99fe310fb96a81fd5f823c944c7
Arie Roos <ariechildofgod@gmail.com>
2025-05-20 09:22:06 +0200

    Change font again

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,7 +20,7 @@ end)
 local config = wezterm.config_builder()
 
 config.color_scheme = "Night Owl (Gogh)"
-config.font = wezterm.font("DaddyTimeMono Nerd Font Mono")
+config.font = wezterm.font("FantasqueSansM Nerd Font Mono")
 config.font_size = 11
 
 config.use_fancy_tab_bar = false
