24f8d7025b56055152acf564a9b1560a5ae53f40
arie <ariechildofgod@gmailcom>
2025-07-15 21:38:56 +0200

    Remove oil open command

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -1,25 +1,8 @@
-local oil_window_id
-
-local function toggle_oil()
-  if oil_window_id ~= nil then
-    vim.api.nvim_win_close(oil_window_id, true)
-    oil_window_id = nil
-    return nil
-  end
-
-  vim.api.nvim_command 'vert topleft 32vs'
-  vim.api.nvim_command 'Oil'
-  oil_window_id = vim.api.nvim_get_current_win()
-end
-
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type function|oil.SetupOpts
-  opts = function()
-    vim.keymap.set('n', '\\', toggle_oil, { desc = 'Open File Explorer' })
-    return {}
-  end,
+  opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
