fa70b12a2de4894ed3a138e03f442011b81b8d03
arie <ariechildofgod@gmailcom>
2025-07-16 11:27:32 +0200

    Improve Oil layout

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type function|oil.SetupOpts
-  opts = {},
+  opts = {
+    columns = { 'icon', 'size', 'mtime' },
+    view_options = {
+      show_hidden = true,
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
