317ef91b6f07440b09fad63aa5abf6be1a0099df
arie <ariechildofgod@gmailcom>
2025-07-18 15:10:38 +0200

    Add local config

---
+++ nvim/lua/custom/plugins/local-config.lua
@@ -0,0 +1,16 @@
+return {
+  'klen/nvim-config-local',
+  config = function()
+    require('config-local').setup {
+      config_files = { '.nvim.lua' },
+
+      -- Where the plugin keeps files data
+      hashfile = vim.fn.stdpath 'data' .. '/config-local',
+
+      autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+      commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalDeny)
+      silent = false, -- Disable plugin messages (Config loaded/denied)
+      lookup_parents = false, -- Lookup config files in parent directories
+    }
+  end,
+}
